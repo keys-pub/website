e227e3d69dac1c8f95133bfd4d7ffe279095c2fb
Gabriel Handford <gabrielh@gmail.com>
2020-09-04 09:23:06 -0700

    Use package vuepress

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 find ../../keys-server/public/docs -mtime +30 -exec rm {} \;
 find ../../keys-server/public/assets -mtime +30 -exec rm {} \;
 
-vuepress build
+yarn vuepress build
 
 cp -R .vuepress/dist/* ../../keys-server/public
